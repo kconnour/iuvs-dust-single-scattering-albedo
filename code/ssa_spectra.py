379d3412d9ebcce3f1e60dd187c0d09c980ceed3
kyle <kdconey1@gmail.com>
2021-09-27 18:23:35 -0600

    Move CRISM plot to the corner of the IUVS plot

--- code/ssa_spectra.py
+++ code/ssa_spectra.py
@@ -4,13 +4,8 @@ import matplotlib.lines as lines
 import numpy as np
 from astropy.io import fits
 
-# TODO: match Icarus fonts + line thicknesses (gulliver)
-# TODO: 2 column legend?
-# TODO: trim down the white space, particularly at the top
-# TODO: Fix the IUVS section to just read in the answer
 
-
-def setup_plot():
+def set_rc_params():
     plt.rc('pdf', fonttype=42)  # ???
     plt.rc('ps', fonttype=42)  # postscript
     font_size = 8
@@ -40,24 +35,18 @@ def setup_plot():
 
 def make_cividis_colors():
     cividis = plt.get_cmap('cividis')
-    return list(cividis(0.1)), list(cividis(0.35)), list(cividis(0.6)), \
-        list(cividis(0.85))
+    return list(cividis(0)), list(cividis(0.1)), list(cividis(0.35)), \
+        list(cividis(0.6)), list(cividis(0.85))
 
 
 if __name__ == '__main__':
-    setup_plot()
-    color_14, color_16, color_18, color_20 = make_cividis_colors()
+    set_rc_params()
 
-    fig = plt.figure()
-    crism_data = plt.axes([0.75, 0.75, 0.2, 0.2])
-    whole = plt.axes([0, 0, 1, 1])
-    whole.patch.set_alpha(0)
-    iuvs_data = plt.axes([0.1, 0.1, 0.85, 0.6])
-    iuvs_data.patch.set_alpha(0.85)
+    fig = plt.figure(figsize=(7.6, 4))
+    iuvs_data = fig.add_axes([0.07, 0.1, 0.88, 0.85], alpha=0.35)
+    crism_data = fig.add_axes([0.7375, 0.16, 0.19, 0.25])
 
-    #whole = fig.add_axes([0, 0, 1, 1])
-    #iuvs_data = fig.add_axes([0.1, 0.1, 0.7, 0.7])
-    #crism_data = fig.add_axes([0.85, 0.85, 0.1, 0.1])
+    color_12, color_14, color_16, color_18, color_20 = make_cividis_colors()
 
     # Load IUVS results
     f = np.load(f'/home/kyle/ssa_retrievals/iteration8/new-fsp_new-pf_hapke-wolff_1-4size.npy')
@@ -66,6 +55,7 @@ if __name__ == '__main__':
     i = np.load(f'/home/kyle/ssa_retrievals/iteration8/new-fsp_new-pf_hapke-wolff_2-0size.npy')
     wavs = np.load('/home/kyle/iuvs_wavelengths.npy')
 
+    # Load IUVS uncertainty
     f_hi = np.load(f'/home/kyle/ssa_retrievals/iteration8/new-fsp_new-pf_hapke-wolff_1-4size-highUnc.npy')
     f_lo = np.load(f'/home/kyle/ssa_retrievals/iteration8/new-fsp_new-pf_hapke-wolff_1-4size-lowUnc.npy')
     g_hi = np.load(f'/home/kyle/ssa_retrievals/iteration8/new-fsp_new-pf_hapke-wolff_1-6size-highUnc.npy')
@@ -116,16 +106,17 @@ if __name__ == '__main__':
     iuvs_data.fill_between(wavs, i + i_hi, i - i_lo, color=color_20, alpha=0.5, linewidth=0)
 
     # Add MARCI results
-    iuvs_data.scatter([258, 320], [0.619, 0.648], color=color_16)
-    iuvs_data.scatter([258, 320], [0.625, 0.653], color=color_18)
-    iuvs_data.errorbar(258, 0.619, yerr=0.010, elinewidth=1, capthick=1,
-                       capsize=3, color=color_16, alpha=0.6)
-    iuvs_data.errorbar(320, 0.648, yerr=0.005, elinewidth=1, capthick=1,
-                       capsize=3, color=color_16, alpha=0.6)
-    iuvs_data.errorbar(258, 0.625, yerr=0.011, elinewidth=1, capthick=1,
-                       capsize=3, color=color_18, alpha=0.6)
-    iuvs_data.errorbar(320, 0.653, yerr=0.005, elinewidth=1, capthick=1,
-                       capsize=3, color=color_18, alpha=0.6)
+    marci_wavelength_offset = 0.3
+    iuvs_data.scatter([258-marci_wavelength_offset, 320 - marci_wavelength_offset], [0.619, 0.648], color=color_16, zorder=4)
+    iuvs_data.scatter([258 + marci_wavelength_offset, 320 + marci_wavelength_offset], [0.625, 0.653], color=color_18, zorder=4)
+    iuvs_data.errorbar(258-marci_wavelength_offset, 0.619, yerr=0.010, elinewidth=1, capthick=1,
+                       capsize=3, color=color_16, alpha=0.6, zorder=3)
+    iuvs_data.errorbar(320-marci_wavelength_offset, 0.648, yerr=0.005, elinewidth=1, capthick=1,
+                       capsize=3, color=color_16, alpha=0.6, zorder=3)
+    iuvs_data.errorbar(258+ marci_wavelength_offset, 0.625, yerr=0.011, elinewidth=1, capthick=1,
+                       capsize=3, color=color_18, alpha=0.6, zorder=3)
+    iuvs_data.errorbar(320 + marci_wavelength_offset, 0.653, yerr=0.005, elinewidth=1, capthick=1,
+                       capsize=3, color=color_18, alpha=0.6, zorder=3)
 
     # Add the legend
     dust_14 = patches.Patch(color=color_14, label=r'1.4 ' + u'\u03bc' + 'm dust')
@@ -151,39 +142,27 @@ if __name__ == '__main__':
     iuvs_data.set_xlabel('Wavelength (nm)')
     iuvs_data.set_ylabel('Single scattering albedo')
 
-    # CRISM
-    cwav = [500, 600, 700, 800, 900, 1000]
-    cssa = [0.83, 0.94, 0.97, 0.968, 0.965, 0.962]
+    # Subplot
+    crism_ssa = np.loadtxt('/home/kyle/repos/iuvs-dust-single-scattering-albedo/data/fig12.dat', skiprows=1)
 
+    # Plot IUVS data on the subplot
     crism_data.plot(wavs, f, color=color_14, linewidth=0.5)
     crism_data.plot(wavs, g, color=color_16, linewidth=0.5)
     crism_data.plot(wavs, h, color=color_18, linewidth=0.5)
     crism_data.plot(wavs, i, color=color_20, linewidth=0.5)
 
+    # Plot CRISM data on the subplot
+    crism_data.plot(crism_ssa[:, 0], crism_ssa[:, 1], color=color_12, linewidth=0.5)
+    crism_data.plot(crism_ssa[:, 0], crism_ssa[:, 3], color=color_14, linewidth=0.5)
+    crism_data.plot(crism_ssa[:, 0], crism_ssa[:, 5], color=color_16, linewidth=0.5)
+    crism_data.plot(crism_ssa[:, 0], crism_ssa[:, 7], color=color_18, linewidth=0.5)
+
     crism_data.axvline(325, ymax=0.06/0.4, color='k', linewidth=0.5)
     crism_data.axhline(0.66, xmax=125/800, color='k', linewidth=0.5)
-
-    crism_data.plot(cwav, cssa, color='k')
 
     crism_data.set_xlim(200, 1000)
     crism_data.set_ylim(0.6, 1)
     crism_data.set_xticks([200, 600, 1000])
     crism_data.set_xticks(np.linspace(200, 1000, num=9), minor=True)
-    crism_data.xaxis.tick_top()
-    crism_data.yaxis.set_label_position('right')
-    crism_data.yaxis.tick_right()
-
-    # Connector lines
-    a = np.array([0.7, 0.95])
-    b = np.array([0.1, 0.85])
-
-    # crism, iuvs
-    whole.plot([0.78125, 0.95], [0.78, 0.7], color='k', linewidth=0.5, linestyle='--')
-    whole.plot([0.75, 0.1], [0.78, 0.7], color='k', linewidth=0.5,linestyle='--')
-    whole.plot([0.75, 0.1], [0.75, 0.1], color='k', linewidth=0.5,linestyle='--')
-    whole.plot([0.78125, 0.95], [0.75, 0.1], color='k', linewidth=0.5,linestyle='--')
-
-    whole.set_xlim(0, 1)
-    whole.set_ylim(0, 1)
 
     plt.savefig(f'/home/kyle/repos/iuvs-dust-single-scattering-albedo/figures/ssa_spectra.pdf')
